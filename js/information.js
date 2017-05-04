caf282e5308279508ccaf70e7c612b6ca728f7d3
Jacob Rieger <JRieger@mcjvs.local>
2017-05-04 11:36:35 -0400

    Added javascript file for holding variables

---
+++ js/information.js
@@ -0,0 +1,24 @@
+var aboutAuthor = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus
+est ac mauris congue, ut molestie sapien porta. Cras eget est at sem
+lacinia accumsan. Suspendisse sit amet ipsum rhoncus, iaculis dui id,
+consectetur urna. Curabitur faucibus elementum justo quis ullamcorper.
+Proin laoreet consectetur ipsum, sit amet mollis felis iaculis non.
+Ut sed tempor neque. Aliquam sed neque malesuada, sagittis ex sit amet,
+tempor nisl. Ut tellus arcu, sollicitudin non magna sit amet, convallis
+elementum nunc. Nullam justo libero, malesuada non magna eget, viverra
+bibendum nisl. Mauris vitae aliquet sapien, nec euismod metus. Quisque
+vulputate dui orci, et accumsan justo aliquam in. Donec non enim a diam
+placerat vulputate. Sed ullamcorper, metus id tincidunt imperdiet, felis
+metus eleifend quam, eu vulputate nibh nisl eu lacus. Phasellus tortor ex,
+scelerisque gravida pretium quis, mollis sit amet velit. Quisque libero
+est, blandit vitae mi vel, pulvinar egestas urna. Pellentesque habitant
+morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+Curabitur volutpat nulla quis blandit lacinia. Sed justo turpis,
+efficitur quis ex vel, euismod varius erat. Phasellus aliquet mi arcu,
+volutpat accumsan risus porttitor quis. Donec pulvinar velit eu nisi
+scelerisque, non lacinia metus pellentesque. Nam cursus mi nec elit
+iaculis eleifend. Ut ornare rhoncus enim non placerat. Phasellus in
+metus lacus. Maecenas interdum, ex ac efficitur sodales, est ipsum
+varius ex, non consequat nulla nisl at nisl.
+`
