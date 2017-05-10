1a597d520d476fb89e9a71ac88376a3683883744
Jake Rieger <contact.jakerieger@gmail.com>
2017-05-10 15:19:11 -0400

    Final commit

--- js/information.js
+++ js/information.js
@@ -1,9 +1,21 @@
 var aboutAuthor = `
+<p>
 <b>Neil Gaiman</b> was born November 10th, 1960 in Hamphsire, England. He found his
 love for stories as a child, and for his seventh birthday he received C.S.
 Lewis's The Chronicles of Narnia. Gaiman loved libraries and credits librarians
 for encouraging the growth of his love for books. He'd spend entire days there
 as a kid.
+</p>
+<center style="font-size: 16pt; color: #8c8b8b">⁕ ⁕ ⁕</center>
+<p>
+Gaiman's professional career started in the early 1980s as a journalist for the
+British Fantasy Society, which he reviewed for copiously. After his first publication
+in 1984, <i>Featherquest</i>, he wrote his first book, a biography of Duran Duran.
+Gaiman would later go on to write his first novel, although not a solor project,
+<i>Good Omens</i>, in 1990. In 1996, Gaiman would release his first solo novel,
+<i>Neverwhere</i> along with the accompanying mini-series on BBC. The novel would be
+revised three times and contains some notable differences from the teleplay.
+</p>
 `
 
 var synopsis = `
@@ -14,16 +26,84 @@ a beautiful but demanding fiancee. On their way to an important dinner, Richard 
 a young girl that appears to be injured. Richard decides to take the girl in to his care
 and the story quickly unfolds from there.
 <p>
-
+<center style="font-size: 16pt; color: #8c8b8b">⁕ ⁕ ⁕</center>
 <p>
 Richard comes to know the girl's name is Door and that she's from London Below. After
 doing a favor for her, his live is turned upside down and he finds himself invisible
-to everyone around him. He loses his job, apartment, and any proof he every existed.
+to everyone around him. He loses his job, apartment, and any proof he ever existed.
 Eventually Richard makes the decision to return to London Below in hopes of finding Door.
-After reuniting with Door, Richard finds himself engulfed in a quest to unlock the secrets
-of Door's family murder and finds himself in situations he could never imagine.
+After reuniting with Door, Richard finds himself enveloped in a quest to unlock the secrets
+of Door's family murder and ends up in situations he could never imagine.
 </p>
 `
 var worksCited = `
+<p>
+"Biography." Neil Gaiman | About Neil | Biography. N.p., n.d. Web. 10 May 2017.
+</p>
+<p>
+GAIMAN, NEIL. NEVERWHERE. Place of publication not identified: WILLIAM MORROW, 2016. Print.
+</p>
+<p>
+"Neil Gaiman." Wikipedia. Wikimedia Foundation, 10 May 2017. Web. 10 May 2017.
+</p>
+`
 
+var richard = `
+Richard Mayhew is originally from Scotland. He moves to London where for the first
+few years, leads a pretty mundane life. He has a monotanous desk job, and a beautiful
+but cold-demeanored fiancee, Jessica. After coming across a young girl on the streets,
+his life changes forever.
+`
+
+var jessica = `
+Jessica, who hates being called Jess, is Richard's fiancee. She has a 'proper'
+attitude and a cold personality. She believes she has a reputation to uphold and
+holds that above all else. She works in fine art and hosts art exhibits in London.
+`
+
+var door = `
+Door is a young girl from London Below with an eccentric ability. She has the ability
+to open any door, create doors, or open doors that never existed. When her family is
+murdered, she finds herself in London Above sitting on the side of the street where
+Richard finds her and takes her to his home. After that, Door leads a quest to uncover
+why her family was murdered.
+`
+
+var marquis = `
+The Marquis de Carabas, also known simply as The Marquis is a character rather
+shrouded in mystery. He's a shady "problem solver", for lack of a better term.
+He spends the majority of the story as a tag along with Door and Richard on the mission,
+where he helps them in various situations.
+`
+
+var hunter = `
+Hunter is a renowned assassin in the underground society. She has famously hunted
+and killed many of the beasts of each underground city and is in London Below to
+hunt the Beast of London. She becomes Door's bodyguard for the majority of the mission
+by winning the audition The Marquis hosted at The Floating Market.
+`
+
+var croup = `
+Mr. Croup is a logophile and a highly skilled assassin. He's short and bulky with
+noticeably orange hair. He has a right hand man named Mr. Vandemar. Throughout the
+novel, he never does the killing himself, but is very clearly the mastermind behind
+all of their ploys.
+`
+
+var vandemar = `
+Mr. Vandemar is quite the opposite of Mr. Croup. He's simple-minded, always hungry,
+and physically much different. He's very tall with dark hair and no distinguishable
+features. Mr. Vandemar is the executor of the duo's operation, doing all the killing.
+`
+
+var islington = `
+The Angel Islington is an angel who's been exiled for destroying Atlantis. After being
+locked away in a room until it's forgiven for what it has done, it hatches a plan to
+escape early and take over Heaven. Islington is ultimately behind the murder of Door's
+family, namely her father because it knows Door can open the door to Heaven for it.
+`
+
+var credits = `
+<p>Design and Code by Jake Rieger</p>
+<p>Fonts by Google</p>
 `
