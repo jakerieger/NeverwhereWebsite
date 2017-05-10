096fa350806e3bb563d39b74896efaff54308974
Jacob Rieger <JRieger@mcjvs.local>
2017-05-10 11:36:37 -0400

    Added works cited

--- js/information.js
+++ js/information.js
@@ -1,19 +1,29 @@
 var aboutAuthor = `
 <b>Neil Gaiman</b> was born November 10th, 1960 in Hamphsire, England. He found his
 love for stories as a child, and for his seventh birthday he received C.S.
-Lewis's The Chronicles of Narnia.
+Lewis's The Chronicles of Narnia. Gaiman loved libraries and credits librarians
+for encouraging the growth of his love for books. He'd spend entire days there
+as a kid.
 `
 
 var synopsis = `
-Richard Mayhew decides to leave his life behind in Scotland and move to London.
-A couple years later he has a stodgy job and a beautiful but demanding
-fiancee. Then one night he stumbles across a girl bleeding on the sidewalk.
-He stops to help her --and the life he knows vanishes like smoke.
-`
+<p>
+Richard Mayhew is a young man from Scotland looking to start a new life in England.
+Fast forward a couple years and he leads a pretty ordinary life with a mundane job and
+a beautiful but demanding fiancee. On their way to an important dinner, Richard spots
+a young girl that appears to be injured. Richard decides to take the girl in to his care
+and the story quickly unfolds from there.
+<p>
 
-var plotSummary = `
-Richard Mayhew is an unassuming young businessman living in London, with a dull
-job and a pretty but demanding fiancee. Then one night he stumbles across a girl
-bleeding on the sidewalk. He stops to help her --and the life he knows vanishes
-like smokes.
+<p>
+Richard comes to know the girl's name is Door and that she's from London Below. After
+doing a favor for her, his live is turned upside down and he finds himself invisible
+to everyone around him. He loses his job, apartment, and any proof he every existed.
+Eventually Richard makes the decision to return to London Below in hopes of finding Door.
+After reuniting with Door, Richard finds himself engulfed in a quest to unlock the secrets
+of Door's family murder and finds himself in situations he could never imagine.
+</p>
+`
+var worksCited = `
+
 `
